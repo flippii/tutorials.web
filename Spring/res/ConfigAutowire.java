418aadabd7b33d152ee09883ae76ba6d360e58d4
Simon Scholz <simon.scholz@vogella.com>
2016-05-13 17:21:27 +0200

    [Spring] Fixed package name issues
    Change-Id: Iaa19c04d165e3cdef8895c32fc21353c83f256eb
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- Spring/res/ConfigAutowire.java
+++ Spring/res/ConfigAutowire.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = { "com.vogella" })
+@ComponentScan(basePackages = { "com.vogella.spring.first.di" })
 public class Config {
 
 	@Bean
