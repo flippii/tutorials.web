b62fa51dd80f7259e87658cafdad88361a5294bf
Simon Scholz <simon.scholz@vogella.com>
2016-05-25 23:51:02 +0200

    [Spring] Moved di res to res/di/ folder
    Change-Id: I329381f14ce93c6a09fb001b8a1fef93a698ff15
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- Spring/res/jpa/TodoRepositoryCustomBetweenDateQuery.java
+++ Spring/res/jpa/TodoRepositoryCustomBetweenDateQuery.java
@@ -1,11 +1,3 @@
-package com.vogella.spring.jpa.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vogella.spring.jpa.Todo;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
 	@Query("Select t from Todo t where t.dueDate BETWEEN '2016-01-01' AND '2016-12-31'")
--- Spring/res/jpa/TodoRepositoryFindBySummaryAndDueDate.java
+++ Spring/res/jpa/TodoRepositoryFindBySummaryAndDueDate.java
@@ -1,11 +1,3 @@
-package com.vogella.spring.jpa.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vogella.spring.jpa.Todo;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
 	Optional<Todo> findBySummaryAndDueDate(String summary, Date date);
