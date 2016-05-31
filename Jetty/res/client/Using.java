1a9d92469f64ea5709515fab3723e8c6ef8cf0e9
Lars Vogel <Lars.Vogel@vogella.com>
2016-05-31 10:30:45 +0200

    Adds Jetty

---
+++ Jetty/res/client/Using.java
@@ -0,0 +1,19 @@
+
+// Get a Client
+HttpClient httpClient = new HttpClient();
+
+// Start server
+httpClient.start();
+
+// do a get request
+ContentResponse response = httpClient
+.GET("http://www.vogella.com")
+.get();
+
+// Issue a post request
+ContentResponse response = httpClient
+.POST("http://www.vogella.com/entity/1")
+.param("p", "value")
+.send()
+.get(5, TimeUnit.SECONDS);
+
