efadb2a2c6348302edf064316c6a1ede47c63892
Fabian Pfaff <fabian.pfaff@vogella.com>
2017-01-17 09:59:05 +0100

    Fixes for Javascript Tutorial
    * wording
    * typos
    * code formatting
    * additional infos about variable keywords

--- JavaScript/res/forms/feedback.js
+++ JavaScript/res/forms/feedback.js
@@ -1,31 +1,26 @@
+function updateFormAction() {
 
-function updateFormAction(){
-	
-	document.forms["myform"].elements["webpage"].value = encodeURIComponent(location.href);
-	var value = document.forms["myform"].elements["user"].value;
-	if (value==null||value=="")
-  	{
-  	 	var Textknoten = document.createTextNode("Please fill out all fields");
-  	 	document.getElementById("replace").replaceChild(Textknoten, document.getElementById("replace").firstChild);
-  	
-  	    return false;
-  		}
-	var value = document.forms["myform"].elements["email"].value;
-	if (value==null||value=="")
-  	{
-  		return false;
-  		}
-  	var value = document.forms["myform"].elements["summary"].value;
-	if (value==null||value=="")
-  	{
-  		return false;
-  		}
-  		
-  	var value = document.forms["myform"].elements["comment"].value;
-	if (value==null||value=="")
-  	{
-  		return false;
-  		}
-	return true; 
-	
-}	
+    document.forms["myform"].elements["webpage"].value = encodeURIComponent(location.href);
+    var value = document.forms["myform"].elements["user"].value;
+    if (value == null || value == "") {
+        var Textknoten = document.createTextNode("Please fill out all fields");
+        document.getElementById("replace").replaceChild(Textknoten, document.getElementById("replace").firstChild);
+
+        return false;
+    }
+    var value = document.forms["myform"].elements["email"].value;
+    if (value == null || value == "") {
+        return false;
+    }
+    var value = document.forms["myform"].elements["summary"].value;
+    if (value == null || value == "") {
+        return false;
+    }
+
+    var value = document.forms["myform"].elements["comment"].value;
+    if (value == null || value == "") {
+        return false;
+    }
+    return true;
+
+}
