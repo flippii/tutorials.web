bb3669c51591f887f98672d6ac7087617e697516
Patrik Suzzi <psuzzi@gmail.com>
2017-02-20 23:31:06 +0100

    fix_01 : fix code snippet and link with code repository

--- JavaScript/res/jsdt.demo01/application_02.js
+++ JavaScript/res/jsdt.demo01/application_02.js
@@ -1,7 +1,7 @@
 /** application.js */
 
 // module
-var app = angular.module('jsdtApp', ['']);
+var app = angular.module('jsdtApp', []);
 
 //controllers with injected angular $scope
 app.controller('mainController', function ($scope) {
